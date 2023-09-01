f430f01ded3f3e31fe579e5b019252dc0271fc33
Greg-Farakhutdinov <141477929+Greg-Farakhutdinov@users.noreply.github.com>
2023-09-01 16:59:31 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+﻿#include "TestDBConnection.h"
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    ClassThatUseDB dbconnect;
+    dbconnect.open("root", "1111", "test");
+    dbconnect.createTable();
+    dbconnect.insertTovar("Tribulus", "0.05 kg", "100");
+    dbconnect.insertTovar("Chelidonium", "0.04 kg", "400");
+    dbconnect.printTovar();
+    dbconnect.deleteTable();
+    dbconnect.close();
+
+    ::testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
+
+    return  0;
+}
